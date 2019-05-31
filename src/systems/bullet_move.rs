06b34c2ce426873cb42d2935b9cce5c55544c370
LiteUnder <alxjacobs14@gmail.com>
2019-05-30 18:21:10 -0600

    delete offscreen bullets

--- src/systems/bullet_move.rs
+++ src/systems/bullet_move.rs
@@ -1,25 +1,29 @@
 use amethyst::core::timing::Time;
 use amethyst::core::Transform;
-use amethyst::ecs::{Join, Read, ReadStorage, System, WriteStorage};
-use amethyst::input::InputHandler;
+use amethyst::ecs::{Join, Read, ReadStorage, System, WriteStorage, Entities};
 
 use crate::rogue::{ROOM_HEIGHT, ROOM_WIDTH};
-use crate::rogue::player::{Player, PLAYER_SIZE};
 use crate::rogue::bullet::{BULLET_WIDTH, BULLET_HEIGHT, Bullet};
 
 pub struct BulletSystem;
 
 impl<'s> System<'s> for BulletSystem {
-   type SystemData = (
+    type SystemData = (
+        Entities<'s>,
         WriteStorage<'s, Transform>,
         ReadStorage<'s, Bullet>,
         Read<'s, Time>,
     );
 
-    fn run(&mut self, (mut transforms, bullets, time): Self::SystemData) {
-        for (local, bullet) in (&mut transforms, &bullets).join() {
+    fn run(&mut self, (entities, mut transforms, bullets, time): Self::SystemData) {
+        for (e, local, bullet) in (&entities, &mut transforms, &bullets).join() {
             local.translate_x(bullet.velocity[0] * time.time_scale());
-            local.translate_x(bullet.velocity[1] * time.time_scale());
+            local.translate_y(bullet.velocity[1] * time.time_scale());
+
+            if local.translation().x < BULLET_WIDTH * -0.5 ||
+                local.translation().x > ROOM_WIDTH + BULLET_WIDTH * 0.5 {
+                    entities.delete(e).unwrap();
+                }
         }
     }
 }
